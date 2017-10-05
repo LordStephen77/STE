ddd949f6a44da21411bbf9a524c3f2fd11bb891f
LordStephen77 <lordstephen77@gmail.com>
2017-10-05 14:33:44 +0200

    Added libraries

--- src/it/lordstephen77/ste/Main.java
+++ src/it/lordstephen77/ste/Main.java
@@ -1,6 +1,18 @@
 package it.lordstephen77.ste;
 
-public class Main {
+import java.awt.BorderLayout;
+import java.awt.Font;
+
+import javax.swing.*;
+
+import org.fife.ui.rtextarea.*;
+import org.fife.ui.rsyntaxtextarea.*;
+
+/*
+ * @author Stefano Peris © 2017
+ */
+
+public class Main extends JFrame {
 
 	public static void main(String[] args) {
 
