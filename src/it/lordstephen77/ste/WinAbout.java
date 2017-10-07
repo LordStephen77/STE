12eccfa8672426a47202c6e93523ca5568157746
LordStephen77 <lordstephen77@gmail.com>
2017-10-08 00:10:55 +0200

    WinAbout update

--- src/it/lordstephen77/ste/WinAbout.java
+++ src/it/lordstephen77/ste/WinAbout.java
@@ -7,13 +7,24 @@
 package it.lordstephen77.ste;
 
 import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.*;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.io.File;
+import java.io.IOException;
+
 public class WinAbout extends JFrame {
+	
+	final String strURL = "https://github.com/LordStephen77/STE";
+	
+	final JEditorPane htmlPane = new JEditorPane();
 	
 	private static final long serialVersionUID = 1L;
 		
@@ -25,26 +36,44 @@ public class WinAbout extends JFrame {
 	        panel.setBackground(new Color(102, 205, 170));
 	        panel.setLayout(null);
 
+	        panel.setBorder(BorderFactory.createTitledBorder(""));
+	        
 	        JLabel title = new JLabel("STE");
 	        title.setBounds(0, 0, 265, 64);
-	        title.setFont(new Font("Arial", Font.BOLD, 14));
+	        title.setFont(new Font("Arial", Font.BOLD, 18));
 	        title.setHorizontalAlignment(SwingConstants.CENTER);
 	        panel.add(title, "push, align center");
 	        
 	        JLabel subtitle = new JLabel("(Script Text Editor)");
 	        subtitle.setBounds(0, 20, 265, 64);
-	        subtitle.setFont(new Font("Arial", Font.BOLD, 14));
+	        subtitle.setFont(new Font("Arial", Font.BOLD, 12));
 	        subtitle.setHorizontalAlignment(SwingConstants.CENTER);
 	        panel.add(subtitle, "push, align center");
 	        
 	        JLabel copyright = new JLabel("By Stefano Peris (c) 2017");
-	        panel.setBorder(BorderFactory.createTitledBorder(""));
-	        copyright.setBounds(-0, 40, 265, 64);
+	        copyright.setBounds(0, 40, 265, 64);
 	        copyright.setFont(new Font("Arial", Font.BOLD, 11));
 	        copyright.setHorizontalAlignment(SwingConstants.CENTER);
-	        panel.add(copyright);
+	        panel.add(copyright, "push, align center");
 	        
-
+	        JLabel note1 = new JLabel("This software is opensource.");
+	        note1.setBounds(0, 80, 265, 64);
+	        note1.setFont(new Font("Arial", Font.BOLD, 11));
+	        note1.setHorizontalAlignment(SwingConstants.CENTER);
+	        panel.add(note1);
+	        
+	        JLabel note2 = new JLabel("Licensed under GPL3.");
+	        note2.setBounds(0, 100, 265, 64);
+	        note2.setFont(new Font("Arial", Font.BOLD, 11));
+	        note2.setHorizontalAlignment(SwingConstants.CENTER);
+	        panel.add(note2);
+	        
+	        JLabel github = new JLabel("♥ Github ♥");
+	        github.setBounds(0, 140, 265, 64);
+	        github.setFont(new Font("Arial", Font.BOLD, 14));
+	        github.setHorizontalAlignment(SwingConstants.CENTER);
+	        panel.add(github);
+	        
 	        UIManager.put("OptionPane.minimumSize",new Dimension(600, 290));
 	        JOptionPane.showMessageDialog(null, panel, "About", JOptionPane.PLAIN_MESSAGE, icon);
 	}
